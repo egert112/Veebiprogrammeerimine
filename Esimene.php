022e057ab1e8d7c8e8fa285b3852f65daf5b1f7f
Egert Klaamas <egert112@users.noreply.github.com>
2017-09-11 15:59:32 +0300

    esimene.php

--- Esimene.php
+++ Esimene.php
@@ -1,37 +1,58 @@
 <?php
-   //muutujad
-   $myName = "Egert":
-   $myFamilyName = "Klaamas":
-   
-   $hourNow = date("H"):
-   
-   $schoolDayStart = date("d.m.Y") ." " ."8.15"
-   //echo $schoolDayStart:
-   $schoolBegin = strtotime($schoolDayStart):
-   //echo $schoolBegin:
-   $timeNow = strtotime("now"):
-   //echo ($timeNow - $schoolBegin):
-   
-   $minutesPassed = round($timeNow - $schoolBegin) / 60:
-   //echo $minutesPassed:
-   
-   //echo $hourNow:
-   //Võrdlen kellaaega ja annan hinnangu, mis päeva osaga on tegemist (<  >  ==  >=  <=  != )
-   $partOfDay = "":
-   if ( $hourNow < 8) {
-   $partOfDay = "varajane hommik": 
-   }
-   //echo $partOfDay:
-   if ( $hourNow >= 0 and $hourNow < 16 ) {
-	   $partOfDay = "koolipäev":
-   }
-   if ( $hournow >= "vaba aeg":
+	//muutujad
+	$myName = "Andrus";
+	$myFamilyName = "Rinde";
+	
+	$hourNow = date("H");
+	
+	$schoolDayStart = date("d.m.Y") ." " ."8.15";
+	//echo $schoolDayStart;
+	$schoolBegin = strtotime($schoolDayStart);
+	//echo $schoolBegin;
+	$timeNow = strtotime("now");
+	//echo ($timeNow - $schoolBegin);
+	
+	$minutesPassed = round(($timeNow - $schoolBegin) / 60);
+	echo $minutesPassed;
+	
+	//echo $hourNow;
+	//võrdlen kellaaega ja annan hinngu, mis päeva osaga on tegemist (<   >   ==   >=  <=  !=  )
+	$partOfDay = "";
+	if ( $hourNow < 8 ){
+		$partOfDay = "varajane hommik";
+	}
+	//echo $partOfDay;
+	if ( $hourNow >= 8 and $hourNow < 16 ) {
+		$partOfDay = "koolipäev";
+	}
+	if ( $hourNow >= 16 ) {
+		$partOfDay = "vaba aeg";
+	}
+	
 ?>
 
-<?php
-echo "<p>kõige esimene php abil väljastatud sõnum.":
-echo "<p>Täna on":
-echo date ("d.m.Y") .", käes on": .$partOfDay:
-echo ".</p>":
-echo "<p>Lehe avamise hetekl oli kell". date ("H:i:s")."</p>
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Andrus Rinde veebiprogemise asjad
+	</title>
+</head>
+<body>
+	<h1>
+	<?php
+		echo $myName ." " .$myFamilyName;
+	?>
+	</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda tõsiseltvõetavat sisu.</p>
+	<?php
+		echo "<p>Kõige esimene PHP abil väljastatud sõnum.</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", käes on " .$partOfDay;
+		echo ".</p>";
+		echo "<p>Lehe avamise hetkel oli kell " .date("H:i:s") .".</p>";
+	
+	?>
+</body>
+</html>
